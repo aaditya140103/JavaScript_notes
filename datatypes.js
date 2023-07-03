81fe69ac9f0a87ca4d4a44463bf0712ad670c842
aaditya140103 <123093692+aaditya140103@users.noreply.github.com>
2023-07-03 23:20:29 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,41 @@
+//data types
+/**
+ * 1. number
+ * 2.string
+ * 3.Boolean
+ * 4.Array
+ * 5.Objects
+ * 6.Null
+ * 7. Undefined
+ * 8.Symbol
+ * 9. bigInt
+ */
+
+//number data type
+//integer and decimal comes in this data types
+var num = 25;
+let pi = 3.14;//decinal data types
+let sun = 25000;
+console.log(num);
+console.log(pi);
+console.log(sun);
+
+//string
+let name = "Aditya";
+let message = "I love Javascript";
+console.log(name, message);
+
+
+//Boolean datatype
+//gives value either true of false
+let greater = 10>15
+console.log(greater);
+
+//Array
+//use to store the data of same types or different types
+var number =[5,6,7,8,9,10];
+var fruits = ['apple','banana','Strawberry'];
+var people = ['Aditya', '20', true];
+console.log(number);
+console.log(people);
+console.log(fruits);
