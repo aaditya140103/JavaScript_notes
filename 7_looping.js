d16747597b340ea0b06ce171fd682e1e33d8442e
aaditya140103 <123093692+aaditya140103@users.noreply.github.com>
2023-07-03 23:44:21 +0530

    Add files via upload

---
+++ 7_looping.js
@@ -0,0 +1,15 @@
+//looping is used when one has to run the same code for multiple time
+//using counter one can run the loop multiple time
+//for loop coutner is inside the loop
+for (var i = 0; i<= 3; i++){
+    console.log("this is line",i);
+}
+console.log("For loop stopped");
+
+//while loop 
+var z = 5;
+while(z>0){  //one has to add condition for running of while looop
+    console.log(z);
+    z-=1;
+}
+console.log("loop stopped");
