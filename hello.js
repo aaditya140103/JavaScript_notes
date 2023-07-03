271db576a51a1e985357d60c05f401fec19b7620
aaditya140103 <123093692+aaditya140103@users.noreply.github.com>
2023-07-03 13:21:06 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,2 @@
+//using console funciton to print a output in javascript with the help of nodejs installed or you can directly paste the code in any browser developer mode.
 console.log("Hello world");
