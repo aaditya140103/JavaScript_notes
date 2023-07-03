69ac7dd8ac8a02109e46a775d1d0f70a64ef4ac4
aaditya140103 <123093692+aaditya140103@users.noreply.github.com>
2023-07-03 23:22:14 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,15 @@
+//using the conditional statement for codes
+//one has to use if else statement as well as switch statement whenever neccessary
+var light = "red";
+if(light == "Green"){
+    console.log("Drive");
+}
+else if(light == "orange"){
+    console.log("'Get ready'");
+}
+else if(light== "red"){
+    console.log("Don't Drive");
+}
+else{
+    console.log("System Failure go on your own to be alive");
+}
