28b0870c4cd2ffd2ba0bb050beb9a59a55e6198a
aaditya140103 <123093692+aaditya140103@users.noreply.github.com>
2023-07-04 00:21:16 +0530

    Add files via upload
    Using nested loop to generate table from 1 to 20. you can also modify this code to generate as much table as you want.

---
+++ 8_nested_loop.js
@@ -0,0 +1,8 @@
+//using nested loop
+//using nested loop to create the tabel of 19
+for(var i = 1; i<=20;i++){
+    console.log("This is Table of",i);
+    for(var j=0; j<=10; j++){
+        console.log( i, "*", j, "=", i*j);
+    }
+}
